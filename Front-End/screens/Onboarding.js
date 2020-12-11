928f4d034c6829abeb64accaabf96c234d969d77
Sergey B <74406979+boychik-serzh@users.noreply.github.com>
2020-12-11 15:55:40 +0900

    update signIn screen

--- Front-End/screens/Onboarding.js
+++ Front-End/screens/Onboarding.js
@@ -17,24 +17,15 @@ export default class Onboarding extends React.Component {
       <Block flex style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Block flex>
-          <ImageBackground
-            source={Images.Onboarding}
-            style={{ flex: 1,
-              resizeMode: "cover",
-              justifyContent: "center" }}
-          />
           <Block space="between" style={styles.padded}>
-            <Block>
-              {/* <Block middle>
-                <Image source={Images.NowLogo} style={{ width: 115, height: 124, bottom: 200, position: 'absolute' }} />
-              </Block> */}
+            <Block center>
               <Block middle>
                 <Text
                   style={{
                     fontFamily: 'montserrat-regular', bottom: 100, position: 'absolute', letterSpacing: 2, paddingHorizontal: 20, textAlign: 'center'
                   }}
                   bold
-                  color="white"
+                  color="black"
                   size={44}
                 >
                   One Chat
@@ -117,28 +108,17 @@ export default class Onboarding extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.COLORS.BLACK,
-    marginTop: Platform.OS === 'android' ? -HeaderHeight : 0
+    backgroundColor: theme.COLORS.WHITE,
   },
   padded: {
+    flex:1,
+    justifyContent:'center',
     paddingHorizontal: theme.SIZES.BASE * 2,
-    zIndex: 3,
-    position: 'absolute',
-    bottom: Platform.OS === 'android' ? theme.SIZES.BASE * 2 : theme.SIZES.BASE * 3
   },
   button: {
-    width: width - theme.SIZES.BASE * 4,
     height: theme.SIZES.BASE * 3,
     shadowRadius: 0,
-    shadowOpacity: 0
+    shadowOpacity: 0,
+    borderRadius:20
   },
-
-  gradient: {
-    zIndex: 1,
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 66
-  }
 });
